6bd61249e2abcc6dd5252cfd6506392002fa06d2
James Chance <38363488+jameschancetech@users.noreply.github.com>
2020-06-06 20:28:32 +0100

    top social boxes styles added

--- dist/social-media-dashboard.e31bb0bc.js
+++ dist/social-media-dashboard.e31bb0bc.js
@@ -193,6 +193,8 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./index.css");
+
+console.log('Hello World');
 },{"./index.css":"index.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -221,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59308" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56849" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
